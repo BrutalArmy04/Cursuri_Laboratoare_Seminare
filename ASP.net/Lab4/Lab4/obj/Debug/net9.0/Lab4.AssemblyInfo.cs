3c3cbf4870972646e00716771da96e987d21e003
BrutalArmy04 <stefanrotaru05@gmail.com>
2025-11-14 22:15:07 +0200

    14.11.25

--- ASP.net/Lab4/Lab4/obj/Debug/net9.0/Lab4.AssemblyInfo.cs
+++ ASP.net/Lab4/Lab4/obj/Debug/net9.0/Lab4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6053497cb06156e30350f0602b092f9997b8146")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
