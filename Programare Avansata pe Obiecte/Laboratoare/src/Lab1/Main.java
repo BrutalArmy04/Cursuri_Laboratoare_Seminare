2f36944640326a3ce2de80af05a7187728122d36
BrutalArmy04 <stefanrotaru05@gmail.com>
2026-03-03 19:38:44 +0200

    03.03.26

---
+++ Programare Avansata pe Obiecte/Laboratoare/src/Lab1/Main.java
@@ -0,0 +1,47 @@
+//package com.pao.laborator00;
+
+package Lab1;
+
+import java.util.Scanner;
+
+public class Main{
+    int is;
+
+    public static void main(String[] args) {
+        matrice();
+    }
+
+    public static float medie(int[] args){
+        float medie=0;
+        for( int i=0; i< args.length; i++){
+            medie+=(float)args[i]/(args.length);
+        }
+
+        return medie;
+    }
+
+    public static void matrice() {
+        int n, m, sp = 0, ps = 1;
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        m = scanner.nextInt();
+        int[][] matr = new int[n][m];
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++) {
+                matr[i][j] = scanner.nextInt();
+                if (i == j) sp += matr[i][j];
+                if (n - i - j == 1) ps *= matr[i][j];
+            }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matr[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+        System.out.println(sp);
+        System.out.println(ps);
+    }
+}
---
+++ Programare Avansata pe Obiecte/Laboratoare/src/Lab2/AudioBook.java
@@ -0,0 +1,49 @@
+
+package Lab2;
+
+import java.util.Arrays;
+import java.util.Comparator; 
+
+public class AudioBook implements Comparable<AudioBook> {
+
+    private String title;
+    private int lengthInSeconds;
+
+    // constructor
+    public AudioBook(String title, int lengthInSeconds) {
+        this.title = title;
+        this.lengthInSeconds = lengthInSeconds;
+    }
+
+    public int getLengthInSeconds() {
+        return this.lengthInSeconds;
+    }
+
+    @Override 
+    public String toString() {
+        return "AudioBook{" + "title='" + title + '\'' + ", lengthInSeconds=" + lengthInSeconds + '}';
+    }
+
+    @Override 
+    public int compareTo(AudioBook o) {
+        return this.title.compareTo(o.title);
+    }
+    
+    public static void main(String[] args) {
+        AudioBook[] books = {
+            new AudioBook("The Great Gatsby", 3600),
+            new AudioBook("Moby Dick", 5400) 
+        }; 
+        
+        Arrays.sort(books);
+        System.out.println("Books sorted by title:");
+        System.out.println(Arrays.toString(books)); 
+    }
+}
+
+class BookLengthComparator implements Comparator<AudioBook> {
+    @Override 
+    public int compare(AudioBook o1, AudioBook o2) {
+        return o1.getLengthInSeconds() - o2.getLengthInSeconds();
+    }
+}
